c77837e4e611c9274adb3afc7f7efb79c5acec0f
Shamim Ahmed <shamimahmed30082@gmail.com>
2024-04-30 10:51:45 +0600

    configured firebase and added all necessary packages to the pubspec file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:assignments/main.dart';
+import 'package:task_tracker/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
